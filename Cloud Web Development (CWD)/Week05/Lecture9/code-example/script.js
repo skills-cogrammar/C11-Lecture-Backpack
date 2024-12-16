2dfa88ba2ce3334a544a8b63890307a8a33614c5
Bonaventure Ogeto <bonaogeto1@gmail.com>
2024-12-17 00:08:00 +0300

    WK05 LO1

---
+++ Cloud Web Development (CWD)/Week05/Lecture9/code-example/script.js
@@ -0,0 +1,64 @@
+let fruits = ["apple", "mango", "avocado"]
+
+// console.log(fruits)
+
+// fruits.push("banana")
+
+// console.log(fruits[0])
+
+// let name = "Paul"
+
+// for (let num = 1; num <= 10; num++){
+//     if (num == 8) {
+//         continue;
+//     }
+
+//     console.log(num)
+// }
+
+// for (let k = 0; k < fruits.length; k++){
+//     console.log(fruits[k])
+// }
+
+// Write a for loop to calculate the sum of numbers from 1 to 100
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// let totalSum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     totalSum += i;
+// }
+
+// console.log(`The sum of numbers from 1 to 100 is ${totalSum}`);
+
+// let sum = 0;
+
+// for (let num=1; num <= 100; num++) {
+//     sum+=num
+// }
+// console.log(sum)
+
+// let number = 0;
+
+// while (number < 10) {
+//     number++;
+//     console.log(number)
+// }
+
+// let number = -10;
+
+// do {
+//     console.log("I'll run at least once.");
+//     number++;
+// } while (number <= 1);
+
+// inifinite loop
+// let number = 0;
+
+// while (number < 10) {
+//     number--;
+//     console.log(number);
+// }
